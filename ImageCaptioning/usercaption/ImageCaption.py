63040ba69e021dc052f888fee11d0ec604f5727b
AjayKarki <ajaykarki333@gmail.com>
2019-07-30 22:51:07 +0545

    Minor improvements

--- ImageCaptioning/usercaption/ImageCaption.py
+++ ImageCaptioning/usercaption/ImageCaption.py
@@ -8,18 +8,20 @@ from keras.models import Model
 from keras.applications.inception_v3 import preprocess_input
 import numpy as np
 from keras import backend as K
+import tensorflow as tf
+
+# Declare this as global:
+global graph
+graph = tf.get_default_graph()
 
 wordtoix = np.load('usercaption/wordtoix.npy',allow_pickle=True).item()
 ixtoword = np.load('usercaption/ixtoword.npy',allow_pickle=True).item()
-model = load_model('usercaption/sampletest.h5')
-modeltype = "8k"
 
-if modeltype="30k":
-    max_length=74
-else:
-    max_length=34
+max_length=34
+
 def caption(photo):
     K.clear_session()
+    model = load_model('usercaption/sampletest.h5')
     in_text = 'startseq'
     for i in range(max_length):
         sequence = [wordtoix[w] for w in in_text.split() if w in wordtoix]
@@ -39,17 +41,17 @@ def extract_features(filename):
     # model = InceptionV3(weights='imagenet')
     # model = Model(model.input, model.layers[-2].output)
     # model.save('loaded.h5')
-
-    model=load_model('loaded.h5')
+    K.clear_session()
+    feature_extraction=load_model('loaded.h5')
     image = load_img(filename, target_size=(299, 299))
     # convert the image pixels to a numpy array
     image = img_to_array(image)
-	# reshape data for the model
+    # reshape data for the model
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-	# prepare the image for the Inception model
+    # prepare the image for the Inception model
     image = preprocess_input(image)
-	# get features
-    feature = model.predict(image, verbose=0)
+    # get features
+    feature = feature_extraction.predict(image, verbose=0)
     return feature
 def get_caption(pic):
     encoded = extract_features(pic)
